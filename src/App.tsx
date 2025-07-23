ff1a53394d87cdf99ae0a3c06af5048769e9f10d
WoftVN <h.hoangtuan257@gmail.com>
2025-07-24 03:05:53 +0700

    yah

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AdminBoothPage from './pages/AdminBoothPage';
 import BoothQRPage from './pages/BoothQRPage';
 import BoothAllocationPage from './pages/BoothAllocationPage';
 import EndPage from './pages/EndPage';
@@ -18,13 +17,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminAuth><AdminDashboard /></AdminAuth>} />
-        <Route path="/admin/:boothId" element={<AdminAuth><AdminBoothPage /></AdminAuth>} />
         <Route path="/admin/booth-qr" element={<AdminAuth><BoothQRPage /></AdminAuth>} />
-        <Route path="/admin/booth/:boothId/allocate" element={<AdminAuth><BoothAllocationPage /></AdminAuth>} />
         <Route path="/admin/rewards" element={<AdminAuth><RewardManagement /></AdminAuth>} />
         <Route path="/admin/rewards/details" element={<AdminAuth><RewardDetails /></AdminAuth>} />
         <Route path="/admin/end" element={<AdminAuth><EndPage /></AdminAuth>} />
         <Route path="/admin/scores" element={<AdminAuth><ScoreManagement /></AdminAuth>} />
+        <Route path="/admin/:boothId" element={<AdminAuth><BoothAllocationPage /></AdminAuth>} />
         <Route path="/results" element={<ResultsPage />} />
 
         {/* Legacy routes - redirect to new admin routes */}
